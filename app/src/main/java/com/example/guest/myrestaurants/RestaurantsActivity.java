d3fdb1287cbaa1e32e2c92c9c72ee77f8108d03f
jwalsh370 <jwalsh370@gmail.com>
2017-05-30 07:38:37 -0700

    add consumer files to gradle properties and addd properties to git ingnotre.

--- app/src/main/java/com/example/guest/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/example/guest/myrestaurants/RestaurantsActivity.java
@@ -11,19 +11,6 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-//public class RestaurantsActivity extends AppCompatActivity {
-//    private TextView mLocationTextView;
-//    private ListView mListView;
-//    private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro",
-//            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
-//            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
-//            "Lardo", "Portland City Grill", "Fat Head's Brewery",
-//            "Chipotle", "Subway"};
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_restaurants);
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
